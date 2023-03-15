b17a7ac4754a1103cea564c04ac53b2a11b15048
Eduardo Vieira <bolacha0953@gmail.com>
2023-03-15 10:38:53 +0100

    Crear ventana

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #Parte gráfica
+import eel, os
 
+def salir(*kw):
+    os._exit(0)
+
+def main():
+    eel.init('webUI')
+    eel.start('index.html', close_callback=salir)
+
+
+if __name__ == "__main__":
+    main()
